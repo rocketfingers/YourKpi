ac0ab025bb727007b0ca4a99c688dd2d36d3a4ea
Piotr Kacprowicz <p.kacprowicz@enterair.pl>
2020-11-27 16:17:18 +0100

    Dodanie podstawowych widokow czesci i produktow

--- youkpifrontend/src/views/Czesci.vue
+++ youkpifrontend/src/views/Czesci.vue
@@ -1,19 +1,69 @@
 <template>
-  <div />
+  <v-container>
+    <v-layout row wrap elevation-3>
+      <v-flex xs12>
+        <v-toolbar flat color="white">
+          <v-toolbar-title>Części</v-toolbar-title>
+          <v-divider class="mx-2" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-text-field
+            append-icon="search"
+            label="Wyszukaj"
+            single-line
+            hide-details
+            class="elevation-1"
+            v-model="search"
+          ></v-text-field>
+          <v-btn color="primary" dark class="mb-2" @click="addPart()"
+            >Dodaj</v-btn
+          >
+        </v-toolbar>
+      </v-flex>
+      <v-flex xs12>
+        <v-data-table
+          :headers="headers"
+          :items="parts"
+          class="elevation-1"
+          item-key="id"
+          loading="true"
+          search="search"
+        >
+        </v-data-table>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: '',
+  name: 'Czesci',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      headers: [
+        { text: 'Id', value: 'id' },
+        { text: 'Nazwa', value: 'nazwa' },
+        { text: 'GatPodstawowy', value: 'gatPodstawowy' },
+        { text: 'NumerRysNorma', value: 'numerRysNorma' },
+        { text: 'Akcje', value: 'actions' }
+      ],
+      parts: [],
+      search: ''
+    }
   },
   computed: {},
   watch: {},
-  methods: {},
-  created () {},
+  methods: {
+    initialise () {},
+
+    addPart () {
+
+    }
+  },
+  created () {
+    this.initialise()
+  },
   destroyed () {}
 }
 </script>
--- youkpifrontend/src/views/Produkty.vue
+++ youkpifrontend/src/views/Produkty.vue
@@ -1,19 +1,73 @@
 <template>
-  <div />
+  <v-container>
+    <v-layout row wrap elevation-3>
+      <v-flex xs12>
+        <v-toolbar flat color="white">
+          <v-toolbar-title>Produkty</v-toolbar-title>
+          <v-divider class="mx-2" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-text-field
+            append-icon="search"
+            label="Wyszukaj"
+            single-line
+            hide-details
+            class="elevation-1"
+            v-model="search"
+          ></v-text-field>
+          <v-btn color="primary" dark class="mb-2" @click="addProduct()"
+            >Dodaj</v-btn
+          >
+        </v-toolbar>
+      </v-flex>
+      <v-flex xs12>
+        <v-data-table
+          :headers="headers"
+          :items="products"
+          class="elevation-1"
+          item-key="id"
+          loading="true"
+          search="search"
+        >
+        </v-data-table>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: '',
+  name: 'Produkty',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      headers: [
+        { text: 'Id', value: 'id' },
+        { text: 'Typ wyrobu id', value: 'typWyrobuId' },
+        { text: 'NumerRysNorma', value: 'numerRysNorma' },
+        { text: 'DN', value: 'dn' },
+        { text: 'PN', value: 'pn' },
+        { text: 'ANSI', value: 'ansi' },
+        { text: 'Wersja', value: 'wersja' },
+        { text: 'Uszczelnienie', value: 'uszczelnienie' },
+        { text: 'Akcje', value: 'actions' }
+      ],
+      products: [],
+      search: ''
+    }
   },
   computed: {},
   watch: {},
-  methods: {},
-  created () {},
+  methods: {
+    initialise () {},
+
+    addProduct () {
+
+    }
+  },
+  created () {
+    this.initialise()
+  },
   destroyed () {}
 }
 </script>
