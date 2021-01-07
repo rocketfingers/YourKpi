627e64d86b590d0d5f9e3ab617bdc7db77854f5b
Miroslaw Siwecki <m.siwecki@enterair.pl>
2021-01-07 10:34:57 +0100

    Merge branch 'master' of https://github.com/rocketfingers/YourKpi

---
+++ YouKpiBackend/ViewModels/CompleteStepViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YouKpiBackend.ViewModels
+{
+    public class SaveCompleteStepViewModel
+    {
+        public string ProcessId { get; set; }
+        public int OfferLineId { get; set; }
+        public int StepNum { get; set; }
+        public int LiczbaPomiarow { get; set; }
+        public int LiczbaPomiarowNok { get; set; }
+        public bool Zakonczenie { get; set;}
+    }
+}
