60801c0a5a6344a783db12f38f97f9b1c0c6384f
Miroslaw Siwecki <m.siwecki@enterair.pl>
2021-01-07 10:48:17 +0100

    saveStep

--- YouKpiBackend/Controllers/ProductionController.cs
+++ YouKpiBackend/Controllers/ProductionController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Microsoft.VisualStudio.Web.CodeGeneration;
 using YouKpiBackend.DbContexts;
 using YouKpiBackend.ModelsEntity;
 using YouKpiBackend.ViewModels;
@@ -38,6 +39,52 @@ namespace YouKpiBackend.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> SaveStep([FromBody] SaveCompleteStepViewModel model)
+        {
+            
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Bad model");
+            }
+            try
+            {
+                var find = await _dbContext.StepOfferWykonanie.FirstOrDefaultAsync(p => 
+                    p.OfferLineId == model.OfferLineId
+                    && p.Step == model.StepNum 
+                    && p.ProcessId == model.ProcessId);
+
+                if (find != null)
+                {
+                    find.LiczbaPomiarowNok = model.LiczbaPomiarowNok;
+                    find.LiczbaPomiarow = model.LiczbaPomiarow;
+                    find.Zakonczonie = model.Zakonczenie;
+                }
+                else
+                {
+                    var itemToAdd = new StepOfferWykonanie()
+                    {
+                        LiczbaPomiarow = model.LiczbaPomiarow,
+                        LiczbaPomiarowNok = model.LiczbaPomiarowNok,
+                        Zakonczonie = model.Zakonczenie,
+                        ProcessId = model.ProcessId,
+                        OfferLineId = model.OfferLineId,
+                        Step = model.StepNum
+                    };
+                    _dbContext.StepOfferWykonanie.Add(itemToAdd);
+                }
+                await _dbContext.SaveChangesAsync();
+
+                //var res = await _ctx.Offer.Include(p => p.Clients).Include(p => p.OfferLines).ThenInclude(p => p.Product).ToListAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
         [HttpGet("[action]")]
         public async Task<IActionResult> GetMainPage()
         {
@@ -73,7 +120,7 @@ namespace YouKpiBackend.Controllers
                     LiczbaPomiarow = 0,
                     LiczbaPomiarowNok = 0,
                     Zakonczone = false
-                });
+                }).ToList();
                 foreach (var res in response)
                 {
                     var wykonanie = wykonanieList.FirstOrDefault(p => p.Step == res.StepNum);
