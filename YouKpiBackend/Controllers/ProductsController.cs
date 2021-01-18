f8b04ad2cc49b8571fdf2f6294fa06ff11d586cc
Miroslaw Siwecki <m.siwecki@enterair.pl>
2021-01-18 11:46:15 +0100

    Poprawka products, ustawienie parenta na null

--- YouKpiBackend/Controllers/ProductsController.cs
+++ YouKpiBackend/Controllers/ProductsController.cs
@@ -112,6 +112,7 @@ namespace YouKpiBackend.Controllers
                 });
                 entity.ProduktCzesci.ToList().ForEach(part =>
                 {
+                    part.Czesci = null;
                     part.ProduktyId = product.Id;
                     product.ProduktCzesci.Add(part);
                 });
