a71827dde57e969ace0a4709cd98cb7200a52be6
Miroslaw Siwecki <m.siwecki@enterair.pl>
2021-02-09 23:54:40 +0100

    stworzenie kontrolera dla raportów i metody statycznej do poberania loginu i hasla z autentykacji closes #12

---
+++ YouKpiBackend/Helpers/BasicAuth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace YouKpiBackend.Helpers
+{
+    public class BasicAuthHelper
+    {
+        static System.Text.Encoding ISO_8859_1_ENCODING = System.Text.Encoding.GetEncoding("ISO-8859-1");
+        public static (string, string) GetUsernameAndPasswordFromAuthorizeHeader(string authorizeHeader)
+        {
+            string encodedUsernamePassword = authorizeHeader.Substring("Basic ".Length).Trim();
+            string usernamePassword = ISO_8859_1_ENCODING.GetString(Convert.FromBase64String(encodedUsernamePassword));
+
+            string username = usernamePassword.Split(':')[0];
+            string password = usernamePassword.Split(':')[1];
+
+            return (username, password);
+
+        }
+    }
+}
+   
