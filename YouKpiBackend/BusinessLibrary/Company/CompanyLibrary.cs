dfbaa4342848a4e4486e32b92f951d4229c0f887
Piotr Kacprowicz <p.kacprowicz@enterair.pl>
2021-03-29 00:11:43 +0200

    Autoryzacja klient test. Refs #34

---
+++ YouKpiBackend/BusinessLibrary/Company/CompanyLibrary.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using YouKpiBackend.DbContexts;
+using YouKpiBackend.ViewModels;
+
+namespace YouKpiBackend.BusinessLibrary.Company
+{
+    public class CompanyLibrary
+    {
+        private readonly IMapper _mapper;
+        readonly YoukpiContext _context;
+
+        public CompanyLibrary(YoukpiContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<CompanyBasicInfoViewModel> GetUserCompanyInfo()
+        {
+            var companyBasicInfo = await _context.CompanyBasicInfo.FirstOrDefaultAsync();
+
+            return _mapper.Map<CompanyBasicInfoViewModel>(companyBasicInfo);
+        }
+    }
+}
